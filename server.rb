fd9a2f56d62d21e66c6230c250bfab8c06cf573a
Hope Lourie Killcoyne <hkillcoyne@gmail.com>
2015-12-13 17:47:58 -0500

    experiment with routes

--- server.rb
+++ server.rb
@@ -1,7 +1,16 @@
 require 'bundler'
 Bundler.require
-require_relative 'connection'
+require_relative 'connection' #this line links the connection.rb file. We do it separately to make it look nice. Like html having link tags.
 
+# Within any website there are actually a whole bunch of different pages. The server's job is know which page to show when, via paths or routes.
 get '/' do
     erb :index
+end
+
+get '/whoa' do
+    "whooaaa"
+end
+
+get '/hallelujah' do
+    "Happy!"
 end
