0755ac601f88fabbfa91b0f0a092aace349eac19
Hope Lourie Killcoyne <hkillcoyne@gmail.com>
2015-12-14 16:05:32 -0500

    remove pry from post to users

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -5,7 +5,17 @@ class UsersController < ApplicationController
     end
 
     post '/' do
-        binding.pry
+        user = User.new(username: params[:username])
+        user.password = params[:password]
+        user.avatar = params[:avatar]
+        user.birthplace = params[:birthplace]
+        user.first_lang = params[:first_lang]
+        user.dream_job = params[:dream_job]
+        user.if_I_were_fancy = params[:if_I_were_fancy]
+        user.favorite_quote = params[:favorite_quote]
+        user.more_info = params[:more_info]
+        user.save!
+        redirect '/'
     end
 
 end
