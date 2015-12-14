3efcad13f46d2d267a4fc2da0160f0658532d3e1
Hope Lourie Killcoyne <hkillcoyne@gmail.com>
2015-12-14 17:51:42 -0500

    write post route in sessions controller

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -4,4 +4,13 @@ class SessionsController < ApplicationController
         erb :'/sessions/new'
     end
 
+    post '/' do
+        redirect '/sessions/new' unless user = User.find_by(username: params[:username])
+        if user.password == params[:password]
+            session[:current_user] = user.id
+        else
+            redirect '/sessions/new'
+        end
+    end
+
 end
