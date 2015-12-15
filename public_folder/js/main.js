2621fee5d0b23716deda781396d838f38b5eedac
Hope Lourie Killcoyne <hkillcoyne@gmail.com>
2015-12-15 14:46:05 -0500

    write js func to log out

--- public_folder/js/main.js
+++ public_folder/js/main.js
@@ -1,1 +1,10 @@
 console.log("I am connected");
+
+var prepareLogout = function() {
+    var logOutLink = document.getElementById("logout-link");
+    var logOutSubmit = document.getElementById("logout-submit");
+    logOutLink.addEventListener("click", function(e) {
+        e.preventDefault();
+        logOutSubmit.click();
+    });
+};
