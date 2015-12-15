2c3b31bf529375071158cbd634cc059e42cf2859
Hope Lourie Killcoyne <hkillcoyne@gmail.com>
2015-12-15 14:45:19 -0500

    write method to formatting user photo path

--- models/user.rb
+++ models/user.rb
@@ -9,4 +9,10 @@ class User < ActiveRecord::Base
         @password = Password.create(new_password)
         self.password_hash = @password
     end
+
+    def format_photo_path(filename)
+        split = filename.split(".")
+        ext = split.slice!(split.length - 1)
+        "images/#{split.join('.') + rand(1000..9999).to_s}.#{ext}"
+    end
 end
